ce3cdbab074da8ac2e75aad9e3304571dac70b7c
肖兴宇 <1577915064@qq.com>
2017-11-23 10:19:04 +0800

    录像按时间下载

--- hcDemoRealPlay/源.cpp
+++ hcDemoRealPlay/源.cpp
@@ -3,8 +3,6 @@
 #include "Windows.h"
 #include "HCNetSDK.h"
 using namespace std;
-//typedef HWND(WINAPI *PROCGETCONSOLEWINDOW)();
-//PROCGETCONSOLEWINDOW GetConsoleWindow;
 void main() {
 	//---------------------------------------
 	// 初始化
@@ -12,10 +10,6 @@ void main() {
 	//设置连接时间与重连时间
 	NET_DVR_SetConnectTime(2000, 1);
 	NET_DVR_SetReconnect(10000, true);
-	//---------------------------------------
-	// 获取控制台窗口句柄
-	HMODULE hKernel32 = GetModuleHandle("kernel32");
-	//GetConsoleWindow = (PROCGETCONSOLEWINDOW)GetProcAddress(hKernel32, "GetConsoleWindow");
 	//---------------------------------------
 	// 注册设备
 	LONG lUserID;
@@ -27,57 +21,63 @@ void main() {
 		NET_DVR_Cleanup();
 		return;
 	}
-	HWND hWnd = GetConsoleWindow(); //获取窗口句柄
-	NET_DVR_VOD_PARA struVodPara = { 0 };
-	struVodPara.dwSize = sizeof(struVodPara);
-	struVodPara.struIDInfo.dwChannel = 33; //通道号
-	struVodPara.hWnd = hWnd; //回放窗口
-	//struVodPara.struBeginTime.dwYear = 2017; //开始时间
-	//struVodPara.struBeginTime.dwMonth = 11;
-	//struVodPara.struBeginTime.dwDay =21;
-	//struVodPara.struBeginTime.dwHour = 9;
-	//struVodPara.struBeginTime.dwMinute = 0;
-	//struVodPara.struBeginTime.dwSecond = 0;
-	//struVodPara.struEndTime.dwYear = 2017; //结束时间
-	//struVodPara.struEndTime.dwMonth = 11;
-	//struVodPara.struEndTime.dwDay = 21;
-	//struVodPara.struEndTime.dwHour = 10;
-	//struVodPara.struEndTime.dwMinute = 7;
-	//struVodPara.struEndTime.dwSecond = 0;
+	NET_DVR_PLAYCOND struDownloadCond = { 0 };
+	struDownloadCond.dwChannel = 1; //通道号
+	struDownloadCond.struStartTime.dwYear = 2017; //开始时间
+	struDownloadCond.struStartTime.dwMonth = 11;
+	struDownloadCond.struStartTime.dwDay = 22;
+	struDownloadCond.struStartTime.dwHour = 9;
+	struDownloadCond.struStartTime.dwMinute = 50;
+	struDownloadCond.struStartTime.dwSecond = 0;
+	struDownloadCond.struStopTime.dwYear = 2017; //结束时间
+	struDownloadCond.struStopTime.dwMonth = 11;
+	struDownloadCond.struStopTime.dwDay = 22;
+	struDownloadCond.struStopTime.dwHour = 10;
+	struDownloadCond.struStopTime.dwMinute = 7; 
+	struDownloadCond.struStopTime.dwSecond = 0;
 	//---------------------------------------
-	string startTime="2017-11-21 09:00:08", stopTime="2017-11-21 10:00:01";
-	sscanf(startTime.c_str(), "%d-%d-%d   %d:%d:%d", &struVodPara.struBeginTime.dwYear, &struVodPara.struBeginTime.dwMonth, &struVodPara.struBeginTime.dwDay, &struVodPara.struBeginTime.dwHour, &struVodPara.struBeginTime.dwMinute, &struVodPara.struBeginTime.dwSecond) > 0
-		&& sscanf(stopTime.c_str(), "%d-%d-%d   %d:%d:%d", &struVodPara.struEndTime.dwYear, &struVodPara.struEndTime.dwMonth, &struVodPara.struEndTime.dwDay, &struVodPara.struEndTime.dwHour, &struVodPara.struEndTime.dwMinute, &struVodPara.struEndTime.dwSecond) > 0
-		;
-	//按时间回放
+	//按时间下载
 	int hPlayback;
-	hPlayback = NET_DVR_PlayBackByTime_V40(lUserID, &struVodPara);
+	hPlayback = NET_DVR_GetFileByTime_V40(lUserID, "./test.mp4", &struDownloadCond);
 	if (hPlayback < 0)
 	{
-		printf("NET_DVR_PlayBackByTime_V40 fail,last error %d\n", NET_DVR_GetLastError());
+		printf("NET_DVR_GetFileByTime_V40 fail,last error %d\n", NET_DVR_GetLastError());
 		NET_DVR_Logout(lUserID);
 		NET_DVR_Cleanup();
 		return;
 	}
 	//---------------------------------------
-	//开始
+	//开始下载
 	if (!NET_DVR_PlayBackControl_V40(hPlayback, NET_DVR_PLAYSTART, NULL, 0, NULL, NULL))
 	{
-		printf("play back control failed [%d]\n", NET_DVR_GetLastError());
+		printf("Play back control failed [%d]\n", NET_DVR_GetLastError());
 		NET_DVR_Logout(lUserID);
 		NET_DVR_Cleanup();
 		return;
 	}
-	Sleep(1500000); //millisecond
-	if (!NET_DVR_StopPlayBack(hPlayback))
+	int nPos = 0;
+	for (nPos = 0; nPos < 100 && nPos >= 0; nPos = NET_DVR_GetDownloadPos(hPlayback))
 	{
-		printf("failed to stop file [%d]\n", NET_DVR_GetLastError());
+		printf("Be downloading... %d %%\n", nPos); //下载进度
+		Sleep(5000); //millisecond
+	}
+	if (!NET_DVR_StopGetFile(hPlayback))
+	{
+		printf("failed to stop get file [%d]\n", NET_DVR_GetLastError());
 		NET_DVR_Logout(lUserID);
 		NET_DVR_Cleanup();
 		return;
 	}
+	if (nPos<0 || nPos>100)
+	{
+		printf("download err [%d]\n", NET_DVR_GetLastError());
+		NET_DVR_Logout(lUserID);
+		NET_DVR_Cleanup();
+		return;
+	}printf("Be downloading... %d %%\n", nPos);
 	//注销用户
 	NET_DVR_Logout(lUserID);
 	//释放 SDK 资源
-	NET_DVR_Cleanup(); return;
+	NET_DVR_Cleanup();
+	return;
 }
